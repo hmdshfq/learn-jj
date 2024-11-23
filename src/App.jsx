58582145b57e35d48dc9b4b491256e3679936d8d
Hammad Shafiq <hmdshfq@outlook.com>
2024-11-23 21:11:38 +0100

    Add real-world workflows and squash workflow

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,15 @@ Jujutsu has some basic commands:
 - We can look at our work with \`jj st\`.
 - When we are done, we can start our next change with \`jj new\`.
 - We can review our repository's contents with \`jj log\`
+
+## Real-world workflows
+
+### The squash workflow
+
+The workflow goes like this:
+- We describe the work we want to do.
+- We create a new empty change on top of that one.
+- As we produce work we want to put into our change, we use \`jj squash\` to move changes from @ into the change where we described what to do.
 `;
 
     return <Markdown>{markdown}</Markdown>;
