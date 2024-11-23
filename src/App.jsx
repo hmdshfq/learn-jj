0e8fc371f903801e1a12d4cea2cc8ff7fc861c1a
Hammad Shafiq <hmdshfq@outlook.com>
2024-11-23 20:32:01 +0100

    Installed react-markdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Markdown from "react-markdown";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const markdown = `
+# Learn Jujutsu
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Learn Jujutsu</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+## Hello, jj
+
+Jujutsu has some basic commands:
+- Create new repositories with \`jj git init\`.
+- To start working on a new change, use \`jj new\`.
+- To describe a change so humans can understand them, use \`jj describe\`.
+- We can look at our work with \`jj st\`.
+- When we are done, we can start our next change with \`jj new\`.
+`;
+
+    return <Markdown>{markdown}</Markdown>;
 }
 
-export default App
+export default App;
